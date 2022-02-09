455b7604666579a9a836b62b1af18bd54bf50ba3
Dave Caruso <dave@davecode.net>
2022-02-09 08:39:27 -0500

    do not allow handlers to run twice

--- src/Purplet.ts
+++ src/Purplet.ts
@@ -24,6 +24,7 @@ export class Purplet implements IPurplet {
   client: Client;
   rest: REST;
   handlers: Handler[] = [];
+  preRegisteredHandlers: Handler[] = [];
 
   constructor(readonly config: Config) {
     const clientOptions = config.discord?.clientOptions ?? {};
@@ -33,6 +34,8 @@ export class Purplet implements IPurplet {
 
     this.client = new Client(clientOptions as ClientOptions);
     this.rest = new REST({ ...restOptions, version: '9' });
+
+    this.preRegisteredHandlers = config.handlers.concat();
   }
 
   public async init() {
@@ -97,6 +100,7 @@ export class Purplet implements IPurplet {
       throw new Error('Handler already added to a purplet.');
     }
 
+    this.preRegisteredHandlers.push(handler);
     handler.client = this.client;
     handler.rest = this.rest;
     handler.config = this.config;
@@ -106,7 +110,7 @@ export class Purplet implements IPurplet {
   }
 
   public async registerInstance(id: string, instance: HandlerInstance<unknown>) {
-    let handler = this.handlers.find((h) => h.constructor === instance.handlerClass);
+    let handler = this.preRegisteredHandlers.find((h) => h.constructor === instance.handlerClass);
     if (!handler) {
       handler = new instance.handlerClass();
       await this.registerHandler(handler);
