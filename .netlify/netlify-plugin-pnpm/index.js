c95ccff4085c90754231533fb1c6ec71152a2770
Dave Caruso <dave@davecode.net>
2022-07-08 19:25:05 -0400

    attempt two fixing netlify build

--- .netlify/netlify-plugin-pnpm/index.js
+++ .netlify/netlify-plugin-pnpm/index.js
@@ -2,7 +2,7 @@ module.exports = {
   onPreBuild: async ({ utils: { build, run } }) => {
     try {
       await run.command("npm install -g pnpm")
-      await run.command("pnpm install --frozen-lockfile=false")
+      await run.command("pnpm install --store=node_modules/.pnpm-store")
     } catch (error) {
       return build.failBuild(error)
     }
