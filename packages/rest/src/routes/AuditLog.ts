1340ad9ec130dfe0cce445d2a1666411829b93f2
hycord <masen0914@gmail.com>
2022-07-11 02:32:17 -0500

    :hammer: Add  and  routes

--- packages/rest/src/routes/AuditLog.ts
+++ packages/rest/src/routes/AuditLog.ts
@@ -2,12 +2,15 @@ import * as REST from 'discord-api-types/rest';
 import { Routes } from 'discord-api-types/rest';
 import { group, route, type } from '../route-group';
 
+/** https://discord.com/developers/docs/resources/audit-log#get-guild-audit-log. */
+const getGuildAuditLog = route({
+  method: 'GET',
+  route: Routes.guildAuditLog,
+  params: ['guildId'],
+  result: type<REST.RESTGetAPIAuditLogResult>(),
+} as const);
+
 /** Routes on https://discord.com/developers/docs/resources/audit-log. */
 export const auditLog = group({
-  getGuildAuditLog: route({
-    method: 'GET',
-    route: Routes.guildAuditLog,
-    params: ['guildId'] as const,
-    result: type<REST.RESTGetAPIAuditLogResult>(),
-  }),
+  getGuildAuditLog,
 });
--- packages/rest/src/routes/Emoji.ts
+++ packages/rest/src/routes/Emoji.ts
@@ -1,2 +1,57 @@
-import { group } from '../route-group';
-export const emoji = group({});
+import * as REST from 'discord-api-types/rest';
+import { Routes } from 'discord-api-types/rest';
+import { group, route, type } from '../route-group';
+
+/** https://discord.com/developers/docs/resources/emoji#list-guild-emojis. */
+const listGuildEmojis = route({
+  method: 'GET',
+  route: Routes.guildEmojis,
+  params: ['guildId'],
+  result: type<REST.RESTGetAPIGuildEmojiResult[]>(),
+} as const);
+
+/** https://discord.com/developers/docs/resources/emoji#get-guild-emoji. */
+const getGuildEmoji = route({
+  method: 'GET',
+  route: Routes.guildEmoji,
+  params: ['guildId', 'emojiId'],
+  result: type<REST.RESTGetAPIGuildEmojiResult>(),
+} as const);
+
+/** https://discord.com/developers/docs/resources/emoji#create-guild-emoji. */
+const createGuildEmoji = route({
+  method: 'POST',
+  route: Routes.guildEmojis,
+  params: ['guildId'],
+  body: type<REST.RESTPostAPIGuildEmojiJSONBody>(),
+  result: type<REST.RESTPostAPIGuildEmojiResult>(),
+  reason: true,
+});
+
+/** https://discord.com/developers/docs/resources/emoji#modify-guild-emoji. */
+const modifyGuildEmoji = route({
+  method: 'PATCH',
+  route: Routes.guildEmoji,
+  params: ['guildId', 'emojiId'],
+  body: type<REST.RESTPatchAPIGuildEmojiJSONBody>(),
+  result: type<REST.RESTPatchAPIGuildEmojiResult>(),
+  reason: true,
+});
+
+/** https://discord.com/developers/docs/resources/emoji#delete-guild-emoji. */
+const deleteGuildEmoji = route({
+  method: 'DELETE',
+  route: Routes.guildEmoji,
+  params: ['guildId', 'emojiId'],
+  result: type<REST.RESTDeleteAPIGuildEmojiResult>(),
+  reason: true,
+} as const);
+
+/** Routes on https://discord.com/developers/docs/resources/emoji. */
+export const emoji = group({
+  listGuildEmojis,
+  getGuildEmoji,
+  createGuildEmoji,
+  modifyGuildEmoji,
+  deleteGuildEmoji
+});
